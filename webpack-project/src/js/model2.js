99831bb5a47e93069fd3bbb16f8ad639fc64344d
lvbo <937029969@qq.com>
2018-01-23 16:30:48 +0800

    12345

--- webpack-project/src/js/model2.js
+++
@@ -1,9 +0,0 @@
-/*****4*****/
-let x = 1;
-let y = 2;
-function m(){
-    console.log(x+y)
-}
-
-export {x,m};
-export default y;
--- webpack-project/src/js/test.js
+++
@@ -1,9 +0,0 @@
-import o,{x,m} from './model2';
-/****3****/
-(function(){
-    alert(3);
-})();
-
-console.log(o);
-console.log(x);
-m();
